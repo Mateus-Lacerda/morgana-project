87989cd47e85231faa377319489350d16e423b46
João Pedro Gonçalves <jpedro.fsgs@gmail.com>
2026-07-25 23:51:27 -0300

    feat: add new button theme and controls grid, update UI scenes

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,47 +1,6 @@
 extends Control
 
 # --- Dados de controles por input mode ---
-const KEYBOARD_DATA := {
-	"move_label": "Mover",
-	"jump_label": "Pular (até 4x)",
-	"shoot_label": "Disparo Mágico",
-	"aura_label": "Aura",
-}
-
-const JOYPAD_DATA := {
-	"move_label": "Mover",
-	"jump_label": "Pular (até 4x)",
-	"shoot_label": "Disparo Mágico",
-	"aura_label": "Aura",
-}
-
-# Texturas de teclado (placeholder também para gamepad por ora)
-var kb_icons := {
-	"IconLeft": preload("res://assets/ui/imagem seta para esquerda.png"),
-	"IconRight": preload("res://assets/ui/imagem seta para direita.png"),
-	"IconJump": preload("res://assets/ui/imagem seta cima para pulo.png"),
-	"IconShoot": preload("res://assets/ui/mouse clique esquerdo disparo mágico.png"),
-	"IconAura": preload("res://assets/ui/mouse clique direito aura.png"),
-}
-
-# Quando os ícones de gamepad forem criados, basta duplicar este dict
-# com as texturas corretas:
-var pad_icons := kb_icons  # placeholder
-
-@onready var _controls: VBoxContainer = $VBox/ControlsContainer
-@onready var _labels := {
-	"move_label": $VBox/ControlsContainer/MoveRow/MoveLabel,
-	"jump_label": $VBox/ControlsContainer/JumpRow/JumpLabel,
-	"shoot_label": $VBox/ControlsContainer/ShootRow/ShootLabel,
-	"aura_label": $VBox/ControlsContainer/AuraRow/AuraLabel,
-}
-@onready var _icons := {
-	"IconLeft": $VBox/ControlsContainer/MoveRow/IconLeft,
-	"IconRight": $VBox/ControlsContainer/MoveRow/IconRight,
-	"IconJump": $VBox/ControlsContainer/JumpRow/IconJump,
-	"IconShoot": $VBox/ControlsContainer/ShootRow/IconShoot,
-	"IconAura": $VBox/ControlsContainer/AuraRow/IconAura,
-}
 
 func _ready() -> void:
 	$VBox/StartButton.pressed.connect(_on_start_pressed)
@@ -50,22 +9,6 @@ func _ready() -> void:
 	
 	MusicManager.play("intro", true)
 	
-	Input.joy_connection_changed.connect(_on_joy_connection_changed)
-	_update_controls_text()
-
-func _update_controls_text() -> void:
-	var is_pad := Input.get_connected_joypads().size() > 0
-	var data := JOYPAD_DATA if is_pad else KEYBOARD_DATA
-	var icons := pad_icons if is_pad else kb_icons
-
-	for key in _labels:
-		_labels[key].text = data[key]
-
-	for key in _icons:
-		_icons[key].texture = icons[key]
-
-func _on_joy_connection_changed(_device: int, _connected: bool) -> void:
-	_update_controls_text()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
---
+++ scripts/ui/controls_grid.gd
@@ -0,0 +1,59 @@
+extends GridContainer
+class_name ControlsGrid
+
+const KEYBOARD_DATA := {
+	"move_label": "Mover",
+	"jump_label": "Impulso Mágico",
+	"shoot_label": "Disparo Mágico",
+	"aura_label": "Aura",
+}
+
+const JOYPAD_DATA := {
+	"move_label": "Mover",
+	"jump_label": "Impulso Mágico",
+	"shoot_label": "Disparo Mágico",
+	"aura_label": "Aura",
+}
+
+var kb_icons := {
+	"IconLeft": preload("res://assets/ui/imagem seta para esquerda.png"),
+	"IconRight": preload("res://assets/ui/imagem seta para direita.png"),
+	"IconJump": preload("res://assets/ui/imagem seta cima para pulo.png"),
+	"IconShoot": preload("res://assets/ui/mouse clique esquerdo disparo mágico.png"),
+	"IconAura": preload("res://assets/ui/mouse clique direito aura.png"),
+}
+
+var pad_icons := kb_icons  # placeholder
+
+@onready var _labels: Dictionary = {
+	"move_label": $MoveLabel,
+	"jump_label": $JumpLabel,
+	"shoot_label": $ShootLabel,
+	"aura_label": $AuraLabel,
+}
+
+@onready var _icons: Dictionary = {
+	"IconLeft": $MoveIcons/IconLeft,
+	"IconRight": $MoveIcons/IconRight,
+	"IconJump": $IconJump,
+	"IconShoot": $IconShoot,
+	"IconAura": $IconAura,
+}
+
+func _ready() -> void:
+	Input.joy_connection_changed.connect(_on_joy_connection_changed)
+	_update_controls_text()
+
+func _update_controls_text() -> void:
+	var is_pad := Input.get_connected_joypads().size() > 0
+	var data := JOYPAD_DATA if is_pad else KEYBOARD_DATA
+	var icons := pad_icons if is_pad else kb_icons
+
+	for key in _labels:
+		_labels[key].text = data[key]
+
+	for key in _icons:
+		_icons[key].texture = icons[key]
+
+func _on_joy_connection_changed(_device: int, _connected: bool) -> void:
+	_update_controls_text()
